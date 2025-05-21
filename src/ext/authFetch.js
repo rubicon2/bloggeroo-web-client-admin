da142dc3b0d362e6c4f94f4bdb7f7b97b20f0dc9
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-05-21 16:20:26 +0100

    Implement a version of authFetch external to react
    So this can be used in event handlers, e.g. when a
    user clicks a button and it sends a post or delete
    request. That is not something we want triggering
    on every re-render.

---
+++ src/ext/authFetch.js
@@ -0,0 +1,40 @@
+// This can be used in event handlers, or in a component with the useAuthFetch hook wrapped around it.
+export default async function authFetch(url, access, options) {
+  try {
+    // Try fetch with provided access code.
+    let dataResponse = await fetch(url, {
+      headers: {
+        Authorization: access ? 'Bearer ' + access : '',
+      },
+      // E.g. method: 'post'! Body!
+      ...options,
+    });
+
+    // If response is 401, try to get a new access code.
+    let newAccess = null;
+    if (dataResponse.status === 401) {
+      const accessResponse = await fetch(
+        `${import.meta.env.VITE_SERVER_URL}/admin/auth/access`,
+        {
+          method: 'post',
+          credentials: 'include',
+        },
+      );
+
+      if (accessResponse.ok) {
+        const json = await accessResponse.json();
+        newAccess = json.data.access;
+        dataResponse = await fetch(url, {
+          headers: {
+            Authorization: newAccess ? 'Bearer ' + newAccess : '',
+          },
+          ...options,
+        });
+      }
+    }
+    // Return result, and new access code if there is one, so the state can be updated.
+    return { response: dataResponse, access: newAccess };
+  } catch (error) {
+    return { fetchError: error.message };
+  }
+}
