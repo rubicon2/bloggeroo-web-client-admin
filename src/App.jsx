908f0b3b06efe9328e50e1edeb93a2798c94bab3
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-05-21 10:41:28 +0100

    Move router component to its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,16 @@
-import Root from './components/root';
-import Blogs from './components/blogs';
-import Blog, { blogLoader } from './components/blog';
-import Error from './components/error';
-import Comments from './components/comments';
-import Comment, { commentLoader } from './components/comment';
-import LogIn from './components/logIn';
-import Users from './components/users';
-import User, { userLoader } from './components/user';
-
+import AppRouter from './AppRouter';
 import userReducer, { initialState } from './reducers/userReducer';
 import { UserStateContext, UserDispatchContext } from './contexts/UserContext';
 
-import { useReducer, useMemo } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { useReducer } from 'react';
 import './App.css';
 
 function App() {
   const [state, dispatch] = useReducer(userReducer, initialState);
-
-  const router = useMemo(() => {
-    return createBrowserRouter([
-      {
-        path: '/',
-        Component: Root,
-        ErrorBoundary: Error,
-        children: [
-          {
-            ErrorBoundary: Error,
-            children: [
-              {
-                index: true,
-                Component: Blogs,
-              },
-              {
-                path: 'blogs',
-                Component: Blogs,
-              },
-              {
-                path: 'blogs/:blogId',
-                Component: Blog,
-                loader: blogLoader(state.accessToken),
-              },
-              {
-                path: 'comments',
-                Component: Comments,
-              },
-              {
-                path: 'comments/:commentId',
-                Component: Comment,
-                loader: commentLoader(state.accessToken),
-              },
-              {
-                path: 'users',
-                Component: Users,
-              },
-              {
-                path: 'users/:userId',
-                Component: User,
-                loader: userLoader(state.accessToken),
-              },
-              {
-                path: 'log-in',
-                Component: LogIn,
-              },
-            ],
-          },
-        ],
-      },
-    ]);
-  }, [state]);
-
   return (
     <UserStateContext value={state}>
       <UserDispatchContext value={dispatch}>
-        <RouterProvider router={router} />
+        <AppRouter />
       </UserDispatchContext>
     </UserStateContext>
   );
---
+++ src/AppRouter.jsx
@@ -0,0 +1,71 @@
+import Root from './components/root';
+import Blogs from './components/blogs';
+import Blog, { blogLoader } from './components/blog';
+import Error from './components/error';
+import Comments from './components/comments';
+import Comment, { commentLoader } from './components/comment';
+import LogIn from './components/logIn';
+import Users from './components/users';
+import User, { userLoader } from './components/user';
+
+import { UserStateContext } from './contexts/UserContext';
+import { useContext, useMemo } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+export default function AppRouter() {
+  const state = useContext(UserStateContext);
+
+  const router = useMemo(() => {
+    return createBrowserRouter([
+      {
+        path: '/',
+        Component: Root,
+        ErrorBoundary: Error,
+        children: [
+          {
+            ErrorBoundary: Error,
+            children: [
+              {
+                index: true,
+                Component: Blogs,
+              },
+              {
+                path: 'blogs',
+                Component: Blogs,
+              },
+              {
+                path: 'blogs/:blogId',
+                Component: Blog,
+                loader: blogLoader(state.accessToken),
+              },
+              {
+                path: 'comments',
+                Component: Comments,
+              },
+              {
+                path: 'comments/:commentId',
+                Component: Comment,
+                loader: commentLoader(state.accessToken),
+              },
+              {
+                path: 'users',
+                Component: Users,
+              },
+              {
+                path: 'users/:userId',
+                Component: User,
+                loader: userLoader(state.accessToken),
+              },
+              {
+                path: 'log-in',
+                Component: LogIn,
+              },
+            ],
+          },
+        ],
+      },
+    ]);
+  }, [state]);
+
+  return <RouterProvider router={router} />;
+}
