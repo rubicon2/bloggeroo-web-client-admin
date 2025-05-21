2ce909f3923fb9c04de1f67158c1fa76a78f7eec
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-05-21 16:22:32 +0100

    useAuthFetch now uses external authFetch function, reducing duplication

--- src/hooks/useAuthFetch.jsx
+++ src/hooks/useAuthFetch.jsx
@@ -1,58 +1,26 @@
 import { UserDispatchContext, UserStateContext } from '../contexts/UserContext';
 import { useState, useEffect, useContext } from 'react';
+import authFetch from '../ext/authFetch';
 
 export default function useAuthFetch(url) {
-  const state = useContext(UserStateContext);
+  const { accessToken } = useContext(UserStateContext);
   const dispatch = useContext(UserDispatchContext);
   const [response, setResponse] = useState(null);
   const [fetchError, setFetchError] = useState(null);
 
   useEffect(() => {
-    async function authFetch() {
-      try {
-        // Do the fetch.
-        // Doesn't fail even if access has expired? But it must do... but the accessResponse code doesn't get triggered.
-        const fetchResponse = await fetch(url, {
-          headers: {
-            Authorization: state.accessToken
-              ? 'Bearer ' + state.accessToken
-              : '',
-          },
-        });
-
-        // If it fails with a 401, use the refresh token to get a new access token.
-        if (fetchResponse.status === 401) {
-          const accessResponse = await fetch(
-            `${import.meta.env.VITE_SERVER_URL}/auth/access`,
-            {
-              method: 'post',
-              // This will include cookies, but only if compliant with CORS.
-              credentials: 'include',
-            },
-          );
-
-          if (accessResponse.ok) {
-            const json = await accessResponse.json();
-            // Setting new access code will re-run this useEffect callback,
-            // so the original fetch will run again with the new access token.
-            dispatch({
-              type: 'refreshed_access_token',
-              accessToken: json.data.access,
-            });
-          }
-        }
-
-        // Whatever the case, return the original fetch response.
-        // Response body can only be consumed once by the calling component.
-        // Therefore, the body should be consumed in a useEffect callback, that re-runs when the response changes.
-        setResponse(fetchResponse);
-      } catch (error) {
-        setFetchError(error.message);
-      }
+    async function fetchData() {
+      const { response, access, fetchError } = await authFetch(
+        url,
+        accessToken,
+      );
+      setResponse(response);
+      setFetchError(fetchError);
+      if (access)
+        dispatch({ type: 'refreshed_access_token', accessToken: access });
     }
-
-    authFetch();
-  }, [url, state, dispatch]);
+    fetchData();
+  }, [url, accessToken, dispatch]);
 
   return { response, fetchError };
 }
