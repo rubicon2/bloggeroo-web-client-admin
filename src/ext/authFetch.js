bf12fcc4b0d34a347e41fbdc5dc23d8c78dac9eb
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-05-22 11:06:03 +0100

    Fix: headers in options object overwriting authorization header added by authFetch

--- src/ext/authFetch.js
+++ src/ext/authFetch.js
@@ -1,13 +1,15 @@
 // This can be used in event handlers, or in a component with the useAuthFetch hook wrapped around it.
-export default async function authFetch(url, access, options) {
+export default async function authFetch(url, access, options = {}) {
   try {
+    const { headers, ...otherOptions } = options;
     // Try fetch with provided access code.
     let dataResponse = await fetch(url, {
       headers: {
+        ...headers,
         Authorization: access ? 'Bearer ' + access : '',
       },
       // E.g. method: 'post'! Body!
-      ...options,
+      ...otherOptions,
     });
 
     // If response is 401, try to get a new access code.
