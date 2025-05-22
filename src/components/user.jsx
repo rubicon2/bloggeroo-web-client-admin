47ab9dfa97929d69ed3892ec05ffcab90466eae2
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-05-22 14:49:22 +0100

    Implement updating of user info and permissions

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,5 +1,8 @@
+import { useContext, useState } from 'react';
 import DeleteButton from './deleteButton';
-import { useLoaderData, useRouteError } from 'react-router';
+import { useLoaderData, useNavigate, useRouteError } from 'react-router';
+import authFetch from '../ext/authFetch';
+import { UserDispatchContext, UserStateContext } from '../contexts/UserContext';
 
 export function userLoader(accessToken) {
   return async ({ params }) => {
@@ -27,9 +30,79 @@ export function userLoader(accessToken) {
 }
 
 export default function User() {
-  const error = useRouteError();
   const json = useLoaderData();
   const user = json.data.user;
+  const navigate = useNavigate();
+
+  const { accessToken } = useContext(UserStateContext);
+  const dispatch = useContext(UserDispatchContext);
+
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [isBanned, setIsBanned] = useState(user.isBanned);
+  const [isAdmin, setIsAdmin] = useState(user.isAdmin);
+
+  // routeError, fetchError, validationErrors? 3 types of errors? This sucks.
+  // Could initialise error to useRouteError result? Then at least only 2 types.
+  const [error, setError] = useState(useRouteError());
+  const [validationErrors, setValidationErrors] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const haveFieldsChanged =
+    user.email !== email ||
+    user.name !== name ||
+    user.isBanned !== isBanned ||
+    user.isAdmin !== isAdmin;
+
+  async function saveChanges(event) {
+    event.preventDefault();
+    setIsFetching(true);
+    const {
+      response,
+      access,
+      fetchError: authFetchError,
+    } = await authFetch(
+      `${import.meta.env.VITE_SERVER_URL}/admin/users/${user.id}`,
+      accessToken,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'put',
+        body: JSON.stringify({
+          email,
+          name,
+          isBanned,
+          isAdmin,
+        }),
+      },
+    );
+
+    if (access)
+      dispatch({ type: 'refreshed_access_token', accessToken: access });
+    if (authFetchError) setError(authFetchError);
+
+    const responseJson = await response.json();
+    switch (responseJson.status) {
+      case 'success': {
+        navigate('/users');
+        break;
+      }
+      case 'fail': {
+        if (responseJson.data.validationErrors)
+          setValidationErrors(responseJson.data.validationErrors);
+        if (responseJson.data.message)
+          setError(new Error(responseJson.data.message));
+        break;
+      }
+      case 'error': {
+        setError(new Error(responseJson.message));
+        break;
+      }
+    }
+    setIsFetching(false);
+  }
+
   return (
     <>
       {user && (
@@ -37,8 +110,49 @@ export default function User() {
           <h2>
             {user.email} - {user.name}
           </h2>
-          <div>Banned - {'' + user.isBanned}</div>
-          <div>Admin - {'' + user.isAdmin}</div>
+          <label>
+            Email:
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <small>{validationErrors?.email}</small>
+          </label>
+          <label>
+            Name:
+            <input
+              type="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <small>{validationErrors?.name}</small>
+          </label>
+          <div>
+            Banned
+            <input
+              type="checkbox"
+              name="is_banned"
+              checked={isBanned}
+              onChange={(e) => setIsBanned(e.target.checked)}
+            />
+          </div>
+          <div>
+            Admin
+            <input
+              type="checkbox"
+              name="is_admin"
+              checked={isAdmin}
+              onChange={(e) => setIsAdmin(e.target.checked)}
+            />
+          </div>
+          <button
+            type="button"
+            disabled={isFetching || !haveFieldsChanged}
+            onClick={saveChanges}
+          >
+            Save Changes
+          </button>
           <DeleteButton
             url={`${import.meta.env.VITE_SERVER_URL}/admin/users/${user.id}`}
             successRedirect={'/users'}
