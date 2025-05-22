cb1016eec42a2afe7d6d05b82bb88b289152412f
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-05-22 14:28:04 +0100

    Implement updating of comments
    Wow this code is horrible. I must be doing something
    wrong. It just seems overly verbose just to do pretty
    basic things. I think I need to go and look at some
    react and react-router tutorials or something.

--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -30,37 +30,58 @@ export function commentLoader(accessToken) {
 }
 
 export default function Comment() {
-  const error = useRouteError();
   const json = useLoaderData();
   const comment = json.data.comment;
   const navigate = useNavigate();
-  const [text, setText] = useState(comment.text);
+
   const state = useContext(UserStateContext);
   const dispatch = useContext(UserDispatchContext);
-  const [fetchError, setFetchError] = useState(null);
+
+  const [text, setText] = useState(comment.text);
+  const [error, setError] = useState(useRouteError());
+  const [validationErrors, setValidationErrors] = useState(null);
   const [isFetching, setIsFetching] = useState(false);
 
   async function updateComment(event) {
     event.preventDefault();
     setIsFetching(true);
-    try {
-      const { access, fetchError: newFetchError } = await authFetch(
-        `${import.meta.env.VITE_SERVER_URL}/admin/comments/${comment.id}`,
-        state.accessToken,
-        {
-          method: 'put',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          body: new URLSearchParams(new FormData(event.target)),
+    const {
+      response,
+      access,
+      fetchError: newFetchError,
+    } = await authFetch(
+      `${import.meta.env.VITE_SERVER_URL}/admin/comments/${comment.id}`,
+      state.accessToken,
+      {
+        method: 'put',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
-      );
-      if (access)
-        dispatch({ type: 'refreshed_access_token', accessToken: access });
-      navigate('/comments');
-      if (newFetchError) throw newFetchError;
-    } catch (error) {
-      setFetchError(error.message);
+        body: new URLSearchParams(new FormData(event.target)),
+      },
+    );
+    if (access)
+      dispatch({ type: 'refreshed_access_token', accessToken: access });
+    if (newFetchError) setError(newFetchError);
+    if (response?.ok) {
+      const responseJson = await response.json();
+      switch (responseJson.status) {
+        case 'success': {
+          navigate('/comments');
+          break;
+        }
+        case 'fail': {
+          if (responseJson.data.validationErrors)
+            setValidationErrors(responseJson.data.validationErrors);
+          if (responseJson.data.message)
+            setError(new Error(responseJson.data.message));
+          break;
+        }
+        case 'error': {
+          setError(new Error(responseJson.message));
+          break;
+        }
+      }
     }
     setIsFetching(false);
   }
@@ -80,9 +101,11 @@ export default function Comment() {
               value={text}
               onChange={(e) => setText(e.target.value)}
             />
-            <button type="submit">Save</button>
+            <small>{validationErrors?.text}</small>
+            <button type="submit" disabled={isFetching}>
+              Save
+            </button>
           </form>
-          {fetchError && <p>{fetchError}</p>}
           {comment.parentCommentId && (
             <small>
               <Link to={`/comments/${comment.parentCommentId}`}>
@@ -98,7 +121,7 @@ export default function Comment() {
           </DeleteButton>
         </>
       )}
-      {error && <p>{error}</p>}
+      {error && <p>{error.message}</p>}
     </>
   );
 }
