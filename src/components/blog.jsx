72fe475ce737ed1580021d5338899aceaf70273f
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-05-27 15:07:01 +0100

    Implement updating of blogs

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,6 +1,10 @@
 import DeleteButton from './deleteButton';
+import { UserDispatchContext, UserStateContext } from '../contexts/UserContext';
 import useComments from '../hooks/useComments';
-import { useLoaderData, useRouteError } from 'react-router';
+import authFetch from '../ext/authFetch';
+
+import { useContext, useState } from 'react';
+import { useLoaderData, useNavigate, useRouteError } from 'react-router';
 
 export function blogLoader(accessToken) {
   // If blogLoader tries to load a non-existent blog, accessToken is always null?
@@ -32,9 +36,20 @@ export function blogLoader(accessToken) {
 }
 
 export default function Blog() {
-  const error = useRouteError();
   const json = useLoaderData();
   const { blog } = json.data;
+  const navigate = useNavigate();
+
+  const { accessToken } = useContext(UserStateContext);
+  const dispatch = useContext(UserDispatchContext);
+
+  const [title, setTitle] = useState(blog.title);
+  const [body, setBody] = useState(blog.body);
+  const [isFetching, setIsFetching] = useState(false);
+  const [validationErrors, setValidationErrors] = useState(null);
+  const [error, setError] = useState(useRouteError());
+
+  const haveFieldsChanged = title !== blog.title || body !== blog.body;
 
   // Fetch comments in a separate fetch so query can change order, filter out certain ones, etc.
   // Split out into a separate component later.
@@ -42,18 +57,86 @@ export default function Blog() {
     `blogId=${blog?.id}&orderBy=createdAt&sortOrder=desc`,
   );
 
+  async function saveChanges(event) {
+    event.preventDefault();
+    setIsFetching(true);
+    const { response, access, fetchError } = await authFetch(
+      `${import.meta.env.VITE_SERVER_URL}/admin/blogs/${blog.id}`,
+      accessToken,
+      {
+        method: 'put',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title,
+          body,
+        }),
+      },
+    );
+    if (access)
+      dispatch({ type: 'refreshed_access_token', accessToken: access });
+    if (fetchError) setError(fetchError);
+    else {
+      const responseJson = await response.json();
+      switch (responseJson.status) {
+        case 'success': {
+          navigate('/blogs');
+          break;
+        }
+        case 'fail': {
+          if (responseJson.data.validationErrors)
+            setValidationErrors(responseJson.data.validationErrors);
+          if (responseJson.data.message) setError(responseJson.data.message);
+          break;
+        }
+        case 'error': {
+          setError(responseJson.message);
+          break;
+        }
+      }
+    }
+    setIsFetching(false);
+  }
+
   return (
     <>
       {blog && (
         <>
-          <h2>{blog.title}</h2>
+          <label>
+            Title:
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <small>{validationErrors?.title}</small>
+          </label>
+          <small>By {blog.owner.name}</small>
           <DeleteButton
             url={`${import.meta.env.VITE_SERVER_URL}/admin/blogs/${blog.id}`}
           >
             Delete
           </DeleteButton>
-          <small>By {blog.owner.name}</small>
-          <p>{blog.body}</p>
+          <label>
+            Body:
+            <textarea
+              name="body"
+              cols="60"
+              rows="10"
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+            />
+            <small>{validationErrors?.body}</small>
+          </label>
+          <button
+            type="button"
+            disabled={isFetching || !haveFieldsChanged}
+            onClick={saveChanges}
+          >
+            Save Changes
+          </button>
           <h3>Comments</h3>
           {comments?.length > 0 ? (
             <ul>
@@ -66,7 +149,7 @@ export default function Blog() {
           )}
         </>
       )}
-      {error && <p>{error}</p>}
+      {error && <p>{error.message}</p>}
     </>
   );
 }
