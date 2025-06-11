57200b03a9d44ea0f0919d34f80fe0f606e93eba
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-06-11 10:56:36 +0100

    Add ordering by different fields to blogsList
    Author currently doesn't work, since the blog doesn't
    have an author name on it. It has a user id, and the
    user has a name. For this to work, the API will have
    to include the author name in the data returned.

--- src/components/blogs/blogsSearchForm.jsx
+++ src/components/blogs/blogsSearchForm.jsx
@@ -14,30 +14,52 @@ export default function BlogsSearchForm() {
 
   return (
     <form onSubmit={handleForm}>
-      <label>
-        Author:
-        <input type="text" name="author" />
-      </label>
-      <label>
-        Title:
-        <input type="text" name="title" />
-      </label>
-      <label>
-        Body:
-        <input type="text" name="body" />
-      </label>
-      <label>
-        From date:
-        <input type="date" name="fromDate" />
-      </label>
-      <label>
-        To date:
-        <input type="date" name="toDate" />
-      </label>
-      <label>
-        Only Unpublished?
-        <input type="checkbox" name="onlyUnpublished" />
-      </label>
+      <fieldset>
+        <legend>Search</legend>
+        <label>
+          Author:
+          <input type="text" name="author" />
+        </label>
+        <label>
+          Title:
+          <input type="text" name="title" />
+        </label>
+        <label>
+          Body:
+          <input type="text" name="body" />
+        </label>
+        <label>
+          From date:
+          <input type="date" name="fromDate" />
+        </label>
+        <label>
+          To date:
+          <input type="date" name="toDate" />
+        </label>
+        <label>
+          Only Unpublished?
+          <input type="checkbox" name="onlyUnpublished" />
+        </label>
+      </fieldset>
+      <fieldset>
+        <legend>Sort</legend>
+        <label>
+          Sort by:
+          <select name="orderBy" defaultValue="publishedAt">
+            <option value="author">Author (not working)</option>
+            <option value="body">Body text</option>
+            <option value="publishedAt">Publication date</option>
+            <option value="title">Title text</option>
+          </select>
+        </label>
+        <label>
+          Sort order:
+          <select name="sortOrder" defaultValue="desc">
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </label>
+      </fieldset>
       <button type="reset">Clear Filters</button>
       <button type="submit">Search</button>
     </form>
--- src/loaders/blogsLoader.js
+++ src/loaders/blogsLoader.js
@@ -1,15 +1,30 @@
 import authFetch from '../ext/authFetch';
 import responseToJsend from '../ext/responseToJsend';
 import requestToSkipTake from '../ext/requestToSkipTake';
-import requestToSearchStr from '../ext/requestToSearchStr';
+import requestToSearchObj from '../ext/requestToSearchObj';
+import objToSearchStr from '../ext/objToSearchStr';
+
+const defaultSettings = {
+  orderBy: 'publishedAt',
+  sortOrder: 'desc',
+  page: 1,
+};
 
 export default function blogsLoader(accessRef) {
   return async ({ request }) => {
+    // Calculate skip and take from take and page url params.
     const { skip, take } = requestToSkipTake(request);
-    const searchParams = requestToSearchStr(request, ['skip', 'take']);
-    const { response, fetchError } = await authFetch(
+    // Turn into an obj so we can merge with default settings if none set by user.
+    const searchParamsObj = {
+      ...defaultSettings,
+      ...requestToSearchObj(request),
+      skip,
       // Take one extra. If data.blogs.length > take, then we know there is another page of info to fetch.
-      `${import.meta.env.VITE_SERVER_URL}/admin/blogs?skip=${skip}&take=${take + 1}&orderBy=createdAt&sortOrder=desc&${searchParams}`,
+      take: take + 1,
+    };
+    const searchParamsStr = objToSearchStr(searchParamsObj);
+    const { response, fetchError } = await authFetch(
+      `${import.meta.env.VITE_SERVER_URL}/admin/blogs?${searchParamsStr}`,
       accessRef,
     );
     if (fetchError) throw fetchError;
