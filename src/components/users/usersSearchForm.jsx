20a5523f5cb633f553f1bd2a2c7a7e129214d734
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-06-23 11:31:30 +0100

    Add ordering by different fields to users page

--- src/components/users/usersSearchForm.jsx
+++ src/components/users/usersSearchForm.jsx
@@ -12,22 +12,44 @@ export default function UsersSearchForm() {
 
   return (
     <form onSubmit={handleForm}>
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Email:
-        <input type="text" name="email" />
-      </label>
-      <label>
-        Only Admins?
-        <input type="checkbox" name="isAdmin" />
-      </label>
-      <label>
-        Only Banned?
-        <input type="checkbox" name="isBanned" />
-      </label>
+      <fieldset>
+        <legend>Search</legend>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Email:
+          <input type="text" name="email" />
+        </label>
+        <label>
+          Only Admins?
+          <input type="checkbox" name="isAdmin" />
+        </label>
+        <label>
+          Only Banned?
+          <input type="checkbox" name="isBanned" />
+        </label>
+      </fieldset>
+      <fieldset>
+        <legend>Sort</legend>
+        <label>
+          Sort by:
+          <select name="orderBy" defaultValue="email">
+            <option value="name">Name</option>
+            <option value="email">Email</option>
+            <option value="createdAt">Joined date</option>
+            <option value="updatedAt">Updated date</option>
+          </select>
+        </label>
+        <label>
+          Sort order:
+          <select name="sortOrder" defaultValue="asc">
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </label>
+      </fieldset>
       <button type="reset">Clear Filters</button>
       <button type="submit">Search</button>
     </form>
--- src/loaders/usersLoader.js
+++ src/loaders/usersLoader.js
@@ -1,14 +1,30 @@
 import authFetch from '../ext/authFetch';
 import requestToSkipTake from '../ext/requestToSkipTake';
-import requestToSearchStr from '../ext/requestToSearchStr';
 import responseToJsend from '../ext/responseToJsend';
+import requestToSearchObj from '../ext/requestToSearchObj';
+import objToSearchStr from '../ext/objToSearchStr';
+
+const defaultSettings = {
+  orderBy: 'email',
+  sortOrder: 'asc',
+  page: 1,
+};
 
 export default function usersLoader(accessRef) {
   return async ({ request }) => {
+    // Calculate skip and take from take and page url params.
     const { skip, take } = requestToSkipTake(request);
-    const searchParams = requestToSearchStr(request);
+    // Turn into an obj so we can merge with default settings if none set by user.
+    const searchParamsObj = {
+      ...defaultSettings,
+      ...requestToSearchObj(request),
+      skip,
+      // Take one extra. If data.users.length > take, then we know there is another page of info to fetch.
+      take: take + 1,
+    };
+    const searchParamsStr = objToSearchStr(searchParamsObj);
     const { response, fetchError } = await authFetch(
-      `${import.meta.env.VITE_SERVER_URL}/admin/users?skip=${skip}&take=${take + 1}&orderBy=name&sortOrder=asc&${searchParams}`,
+      `${import.meta.env.VITE_SERVER_URL}/admin/users?${searchParamsStr}`,
       accessRef,
     );
     if (fetchError) throw fetchError;
