152055fc9925230cf412332eb8cc0a09b78dcbaf
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-06-23 11:24:17 +0100

    Add ordering by different fields to comments page

--- src/components/comments/commentsSearchForm.jsx
+++ src/components/comments/commentsSearchForm.jsx
@@ -12,22 +12,42 @@ export default function CommentsSearchForm() {
 
   return (
     <form onSubmit={handleForm}>
-      <label>
-        Author:
-        <input type="text" name="author" />
-      </label>
-      <label>
-        Text:
-        <input type="text" name="text" />
-      </label>
-      <label>
-        From date:
-        <input type="date" name="fromDate" />
-      </label>
-      <label>
-        To date:
-        <input type="date" name="toDate" />
-      </label>
+      <fieldset>
+        <legend>Search</legend>
+        <label>
+          Author:
+          <input type="text" name="author" />
+        </label>
+        <label>
+          Text:
+          <input type="text" name="text" />
+        </label>
+        <label>
+          From date:
+          <input type="date" name="fromDate" />
+        </label>
+        <label>
+          To date:
+          <input type="date" name="toDate" />
+        </label>
+      </fieldset>
+      <fieldset>
+        <legend>Sort</legend>
+        <label>
+          Sort by:
+          <select name="orderBy" defaultValue="createdAt">
+            <option value="owner.name">Author</option>
+            <option value="createdAt">Date</option>
+          </select>
+        </label>
+        <label>
+          Sort order:
+          <select name="sortOrder" defaultValue="desc">
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </label>
+      </fieldset>
       <button type="reset">Clear Filters</button>
       <button type="submit">Search</button>
     </form>
--- src/loaders/commentsLoader.js
+++ src/loaders/commentsLoader.js
@@ -1,14 +1,30 @@
 import authFetch from '../ext/authFetch';
-import requestToSearchStr from '../ext/requestToSearchStr';
+import objToSearchStr from '../ext/objToSearchStr';
+import requestToSearchObj from '../ext/requestToSearchObj';
 import requestToSkipTake from '../ext/requestToSkipTake';
 import responseToJsend from '../ext/responseToJsend';
 
+const defaultSettings = {
+  orderBy: 'createdAt',
+  sortOrder: 'desc',
+  page: 1,
+};
+
 export default function commentsLoader(accessRef) {
   return async ({ request }) => {
+    // Calculate skip and take from take and page url params.
     const { skip, take } = requestToSkipTake(request);
-    const searchParams = requestToSearchStr(request, [skip, take]);
+    // Turn into an obj so we can merge with default settings if none set by user.
+    const searchParamsObj = {
+      ...defaultSettings,
+      ...requestToSearchObj(request),
+      skip,
+      // Take one extra. If data.comments.length > take, then we know there is another page of info to fetch.
+      take: take + 1,
+    };
+    const searchParamsStr = objToSearchStr(searchParamsObj);
     const { response, fetchError } = await authFetch(
-      `${import.meta.env.VITE_SERVER_URL}/admin/comments?skip=${skip}&take=${take + 1}&orderBy=createdAt&sortOrder=desc&${searchParams}`,
+      `${import.meta.env.VITE_SERVER_URL}/admin/comments?${searchParamsStr}`,
       accessRef,
     );
     if (fetchError) throw fetchError;
