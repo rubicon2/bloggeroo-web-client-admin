fd5607dcf4e066602b5eebd11059e1088a62be8f
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-06-27 15:01:02 +0100

    Update objToSearchStr to work with arrays of values, as well as single values

--- src/ext/objToSearchStr.js
+++ src/ext/objToSearchStr.js
@@ -2,7 +2,14 @@ export default function objToSearchStr(obj) {
   return Object.entries(obj)
     .map((entry) => {
       const [key, value] = entry;
-      return key + '=' + value;
+      let str = '';
+      // If value is array, turn into multiple query keys and values for the same key.
+      const valueArr = [value].flat();
+      for (const v of valueArr) {
+        str += key + '=' + v + '&';
+      }
+      // Remove last '&'.
+      return str.slice(0, str.length - 1);
     })
     .join('&');
 }
