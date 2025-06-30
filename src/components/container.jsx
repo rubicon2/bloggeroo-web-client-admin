574b04e047279e4aa9a37d53d20db0c292da3c51
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-06-30 10:45:14 +0100

    Simplify container to export styled component, so it can be extended by other styled components

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,11 +1,9 @@
 import styled from 'styled-components';
 
-const ContainerDiv = styled.div`
+const Container = styled.div`
   max-width: 760px;
   margin: 0 auto;
   padding: 0 1rem;
 `;
 
-export default function Container({ children }) {
-  return <ContainerDiv>{children}</ContainerDiv>;
-}
+export default Container;
