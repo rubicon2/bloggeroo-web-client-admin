b8b85238347c1e0b9d17996dc333f1628f431653
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-01 15:26:44 +0100

    Hide form in mobile mode and show a button that toggles form visibility

--- src/components/blogs/blogsPage.jsx
+++ src/components/blogs/blogsPage.jsx
@@ -5,22 +5,43 @@ import BlogsList from './blogsList';
 import PageTitleBar from '../pageTitleBar';
 import { Cols, Sticky } from '../styles/mainPage';
 import { GeneralButton } from '../styles/buttons';
+import { MediaMobileOnly, MediaTabletAndLarger } from '../styles/mediaQueries';
 
 import useSearchParamsPageNumber from '../../hooks/useSearchParamsPageNumber';
 import { Link, useLoaderData, useRouteError } from 'react-router';
+import { useState } from 'react';
 
 export default function BlogsPage() {
   const { blogs, atLastPage } = useLoaderData();
   const error = useRouteError();
   const [currentPageNumber, setCurrentPageNumber] = useSearchParamsPageNumber();
+  const [isMobileSearchOpen, setIsMobileSearchOpen] = useState(false);
+
   return (
     <main>
       <PageTitleBar title="Blogs">
+        <MediaMobileOnly>
+          <GeneralButton
+            type="button"
+            onClick={() => setIsMobileSearchOpen(!isMobileSearchOpen)}
+            aria-label={
+              isMobileSearchOpen ? 'Close search form' : 'Show search form'
+            }
+          >
+            {isMobileSearchOpen ? 'Close' : 'Search'}
+          </GeneralButton>
+        </MediaMobileOnly>
         <Link to="/blogs/new">
           <GeneralButton type="button">New</GeneralButton>
         </Link>
       </PageTitleBar>
       <Container>
+        {isMobileSearchOpen && (
+          // Form seemed sluggish on Firefox but only when touch simulation was turned on?
+          <MediaMobileOnly>
+            <BlogsSearchForm />
+          </MediaMobileOnly>
+        )}
         <Cols>
           <div>
             <BlogsList blogs={blogs} />
@@ -31,11 +52,11 @@ export default function BlogsPage() {
               atLastPage={atLastPage}
             />
           </div>
-          <div>
+          <MediaTabletAndLarger>
             <Sticky>
               <BlogsSearchForm />
             </Sticky>
-          </div>
+          </MediaTabletAndLarger>
         </Cols>
       </Container>
     </main>
