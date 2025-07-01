419bf08687e8a58f308b25c51b97ee4460bbd243
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-01 16:28:11 +0100

    Space out list items

--- src/components/users/usersListUser.jsx
+++ src/components/users/usersListUser.jsx
@@ -1,4 +1,9 @@
 import { Link } from 'react-router';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding-bottom: 1rem;
+`;
 
 export default function UsersListUser({ user }) {
   return (
@@ -6,8 +11,10 @@ export default function UsersListUser({ user }) {
       <h3>
         {user.email} - {user.name}
       </h3>
-      <div>Banned - {'' + user.isBanned}</div>
-      <div>Admin - {'' + user.isAdmin}</div>
+      <Container>
+        <div>Banned - {'' + user.isBanned}</div>
+        <div>Admin - {'' + user.isAdmin}</div>
+      </Container>
     </Link>
   );
 }
