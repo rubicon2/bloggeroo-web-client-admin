3dc2f2f3e0782fdd538ae5925258adbbc9a3ff79
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-01 15:28:39 +0100

    Improve layout of pageTitleBar children

--- src/components/pageTitleBar.jsx
+++ src/components/pageTitleBar.jsx
@@ -7,12 +7,17 @@ const Header = styled.header`
   align-items: center;
 `;
 
+const Children = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
 export default function PageTitleBar({ title = 'Page Title', children }) {
   return (
     <Container>
       <Header>
         <h2>{title}</h2>
-        {children && <div>{children}</div>}
+        {children && <Children>{children}</Children>}
       </Header>
     </Container>
   );
