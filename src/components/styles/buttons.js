b1324d37bf71c9b8fa72c4191c20609d3c3f1c16
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-02 14:29:59 +0100

    Move buttons styling from index.css to styled-components button

--- src/components/styles/buttons.js
+++ src/components/styles/buttons.js
@@ -1,15 +1,26 @@
 import styled from 'styled-components';
 
 const GeneralButton = styled.button`
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  cursor: pointer;
+
+  border-color: transparent;
   background-color: var(--theme-main-color);
   color: white;
 
   &:hover {
     filter: brightness(0.8);
+    border-color: #646cff;
   }
 
-  &:focus {
-    outline: solid 1px black;
+  &:focus,
+  &:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
   }
 `;
 
