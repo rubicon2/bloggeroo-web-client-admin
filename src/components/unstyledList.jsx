d30aab98f91d0a301bbd927be25cf4b76c243bcb
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-03 11:02:40 +0100

    Make unstyledList into an extendable styled-component

--- src/components/unstyledList.jsx
+++ src/components/unstyledList.jsx
@@ -1,11 +1,9 @@
 import styled from 'styled-components';
 
-const List = styled.ul`
+const UnstyledList = styled.ul`
   list-style: none;
   padding: 0;
   margin: 0;
 `;
 
-export default function UnstyledList({ children }) {
-  return <List>{children}</List>;
-}
+export default UnstyledList;
