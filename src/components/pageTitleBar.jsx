49d41a488884e930168196d9cb92ad9fa9577f85
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-03 10:52:27 +0100

    Make sure pageTitleBar buttons fill width of screen on small devices

--- src/components/pageTitleBar.jsx
+++ src/components/pageTitleBar.jsx
@@ -1,15 +1,25 @@
 import Container from './container';
+import { devices } from '../mediaQueries';
 import styled from 'styled-components';
 
 const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr;
+
+  @media ${devices.tablet} {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
 
 const Children = styled.div`
   display: flex;
   gap: 1rem;
+
+  & * {
+    width: 100%;
+  }
 `;
 
 export default function PageTitleBar({ title = 'Page Title', children }) {
