5c65b920a321e009d224ece8e99efffe15f0f106
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-04 11:20:34 +0100

    Make page header responsive to screen width

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,20 @@
 import Container from './container';
+import { devices } from '../mediaQueries';
+import styled from 'styled-components';
+
+const HeaderContainer = styled(Container)`
+  text-align: center;
+  @media ${devices.tablet} {
+    text-align: left;
+  }
+`;
 
 export default function Header() {
   return (
     <header>
-      <Container>
+      <HeaderContainer>
         <h1>Bloggeroo Admin</h1>
-      </Container>
+      </HeaderContainer>
     </header>
   );
 }
