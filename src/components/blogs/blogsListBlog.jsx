4a01e752880d5a4f8d81a0beb8979b976165d7a3
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-04 16:39:24 +0100

    Improve styling of header

--- src/components/blogs/blogsListBlog.jsx
+++ src/components/blogs/blogsListBlog.jsx
@@ -1,15 +1,23 @@
 import ListItemButtonsContainer from '../listItemButtonsContainer';
 import { GeneralButton } from '../styles/buttons';
 import DeleteButton from '../deleteButton';
+import dateTimeFormatter from '../../ext/dateTimeFormatter';
 import { Link } from 'react-router';
+import styled from 'styled-components';
+
+const BlogHeader = styled.h3`
+  margin-bottom: 0;
+`;
 
 export default function BlogsListBlog({ blog, onDelete }) {
   return (
     <div>
-      <h3>{blog.title}</h3>
+      <BlogHeader>{blog.title}</BlogHeader>
       <small>
         By {blog.owner.name} at{' '}
-        {blog.publishedAt ? new Date(blog.publishedAt).toDateString() : 'never'}
+        {blog.publishedAt
+          ? dateTimeFormatter.format(new Date(blog.publishedAt))
+          : 'never'}
       </small>
       <p>{blog.body}</p>
       <ListItemButtonsContainer>
