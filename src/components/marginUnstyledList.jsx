d939f49df7419946d906c39452137f39e4a1e4bc
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-08 12:20:35 +0100

    Create styled-components for improving layout of userPageBlogs and userPageComments

---
+++ src/components/marginUnstyledList.jsx
@@ -0,0 +1,8 @@
+import UnstyledList from './unstyledList';
+import styled from 'styled-components';
+
+const MarginUnstyledList = styled(UnstyledList)`
+  margin-bottom: 1rem;
+`;
+
+export default MarginUnstyledList;
