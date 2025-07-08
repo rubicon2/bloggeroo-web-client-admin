e99d44c36b6da838df57cf569fa29f23f383acb6
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-08 11:45:08 +0100

    Convert form from react component to styled-component so its styles can be extended

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
-const FormContainer = styled.form`
+const Form = styled.form`
   display: grid;
   grid-auto-rows: auto;
   gap: 1rem;
   max-width: 400px;
 `;
 
-export default function Form({ children, ...rest }) {
-  return <FormContainer {...rest}>{children}</FormContainer>;
-}
+export default Form;
+
+// export default function Form({ children, ...rest }) {
+//   return <FormContainer {...rest}>{children}</FormContainer>;
+// }
