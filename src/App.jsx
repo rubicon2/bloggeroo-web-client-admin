2a91650c5222b49e14836c4014890d3d6aecddeb
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-08-08 13:43:06 +0100

    Debug production cookies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import AppRouter from './AppRouter';
 import { AccessContext, UserContext } from './contexts/AppContexts';
 import useLoginState from './hooks/useLoginState';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useLoginState();
   const accessRef = useRef(null);
+
+  // For debugging those cookies which seem to get set, but do not appear on devtools.
+  const intervalRef = useRef(null);
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      console.log('cookie:', document.cookie);
+    }, 5000);
+
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
   return (
     <AccessContext value={accessRef}>
       <UserContext value={{ isLoggedIn, setIsLoggedIn }}>
