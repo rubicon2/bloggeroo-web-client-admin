19a4a22672277f4c0eef5e78df035b91e20d66ca
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-01-13 16:01:54 +0000

    Comonents > UsersListUser: replace DeleteButton component with api DELETE function calls

--- src/components/users/usersList.jsx
+++ src/components/users/usersList.jsx
@@ -2,14 +2,14 @@ import UnstyledList from '../unstyledList';
 import LineSeparatedListItem from '../lineSeparatedListItem';
 import UsersListUser from './usersListUser';
 
-export default function UsersList({ users, onDelete }) {
+export default function UsersList({ users }) {
   return (
     <>
       {users?.length > 0 ? (
         <UnstyledList>
           {users.map((user) => (
             <LineSeparatedListItem key={user.id}>
-              <UsersListUser user={user} onDelete={onDelete} />
+              <UsersListUser user={user} />
             </LineSeparatedListItem>
           ))}
         </UnstyledList>
