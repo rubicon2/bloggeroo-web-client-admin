d45dbe80c12b68fa4f9b4f7a73ec7e03c0fde7e8
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-01-13 16:47:11 +0000

    Components > BlogsListBlog: replace DeleteButton component with api DELETE function calls

--- src/components/blogs/blogsList.jsx
+++ src/components/blogs/blogsList.jsx
@@ -2,14 +2,14 @@ import UnstyledList from '../unstyledList';
 import BlogsListBlog from './blogsListBlog';
 import LineSeparatedListItem from '../lineSeparatedListItem';
 
-export default function BlogsPageList({ blogs, onDelete }) {
+export default function BlogsPageList({ blogs }) {
   return (
     <>
       {blogs?.length > 0 ? (
         <UnstyledList>
           {blogs.map((blog) => (
             <LineSeparatedListItem key={blog.id}>
-              <BlogsListBlog blog={blog} onDelete={onDelete} />
+              <BlogsListBlog blog={blog} />
             </LineSeparatedListItem>
           ))}
         </UnstyledList>
