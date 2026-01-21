53276e1cf44f537143c5c51f621d3fc3a0c4ebf4
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-01-21 11:33:03 +0000

    Components > LineSeparatedListItem: adapt to dark mode

--- src/components/lineSeparatedListItem.jsx
+++ src/components/lineSeparatedListItem.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const LineSeparatedListItem = styled.li`
-  border-bottom: 2px solid var(--theme-grey);
+  border-bottom: 2px solid var(--theme-soft-outline-color);
 
   &:last-of-type {
     border-bottom: 0px;
