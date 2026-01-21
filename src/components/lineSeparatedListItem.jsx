fb3d9886dda4cbc07a4bd6d897b0f2c4875c92d9
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-01-21 11:33:59 +0000

    Components > LineSeparatedListItem: fix uneven spacing of border and neighbouring items

--- src/components/lineSeparatedListItem.jsx
+++ src/components/lineSeparatedListItem.jsx
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
 const LineSeparatedListItem = styled.li`
+  // For even spacing between items.
+  padding-bottom: 1rem;
+  margin-bottom: 1rem;
   border-bottom: 2px solid var(--theme-soft-outline-color);
 
   &:last-of-type {
+    // Since border will be gone, no need for padding and margin.
+    // Remove padding here. That way, if margin doubles up with a following element, margin collapse will occur.
+    padding-bottom: 0;
     border-bottom: 0px;
   }
 `;
